a67e16904f99d68d23739b674a6a2c9551c16c47
bekk-01 <oybekolimov001@gmail.com>
2025-01-23 20:39:47 +0500

    secret key generated

--- src/main/java/org/example/deliveryservice/configuration/JwtTokenUtil.java
+++ src/main/java/org/example/deliveryservice/configuration/JwtTokenUtil.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -15,8 +16,9 @@ import java.util.Date;
 @Component
 public class JwtTokenUtil {
 
-    public static final String SECRET_KEY = "A665A45920422F9D417E4867EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3";
-    public static final Key signignKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(SECRET_KEY));
+    @Value("${jwt.secret.key}")
+    private static String secretKey;
+    public static final Key signignKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
 
     public String generateToken(@NonNull String email) {
         long twentyDaysInMillis = 20 * 24 * 60 * 60 * 1000; // 20 kunni millisekundda hisoblash
@@ -30,25 +32,26 @@ public class JwtTokenUtil {
                 .compact();
     }
 
-    private Claims getClaims(String token){
-       return Jwts.parserBuilder()
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
                 .setSigningKey(signignKey)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
-    public boolean isValid(String token){
+
+    public boolean isValid(String token) {
         try {
             Claims claims = getClaims(token);
             Date expiration = claims.getExpiration();
             return expiration.after(new Date());
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return false;
     }
 
-    public String getEmail(String token){
+    public String getEmail(String token) {
         Claims claims = getClaims(token);
         return claims.getSubject();
     }
