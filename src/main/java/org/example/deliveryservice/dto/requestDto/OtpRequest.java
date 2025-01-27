2f32f8d0cf744559fc4f6c3c3103fb1402a8e279
bekk-01 <oybekolimov001@gmail.com>
2025-01-28 01:26:12 +0500

    update

--- src/main/java/org/example/deliveryservice/dto/requestDto/OtpRequest.java
+++ src/main/java/org/example/deliveryservice/dto/requestDto/OtpRequest.java
@@ -3,12 +3,13 @@ package org.example.deliveryservice.dto.requestDto;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.Value;
 
 import java.io.Serializable;
 
 
-
+@Getter
 public class OtpRequest implements Serializable {
     @Email
     String email;
